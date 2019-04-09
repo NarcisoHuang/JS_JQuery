acc63387901c75b560e7fef4d6c3edc17a68506b
Narciso Huang <huangzhehaocool@163.com>
2019-04-09 18:30:36 +0800

    add js

--- 03_JQuery_JS/项目实践01_图片轮播/js/script.js
+++ 03_JQuery_JS/项目实践01_图片轮播/js/script.js
@@ -6,9 +6,9 @@ function byId(id) {
 
 var index = 0,
     timer = null,
-    pics  = byId("banner").getElementsByTagName("div"),
-    dots  = byId("dots").getElementsByTagName("span");
-    len   = pics.length;
+    pics = byId("banner").getElementsByTagName("div"),
+    dots = byId("dots").getElementsByTagName("span");
+len = pics.length;
 // console.log(len);
 
 function slideImg() {
@@ -16,7 +16,7 @@ function slideImg() {
     // 划过清楚定时器, 离开继续
     main.onmouseover = function () {
         // 划过清除定时器
-        if(timer) {
+        if (timer) {
             clearInterval(timer);
         }
     }
@@ -31,18 +31,20 @@ function slideImg() {
             // console.log(index);
             // 切换图片
             changeImg();  // index是全局变量, 应次不需要传参数
-            
+
         }, 3800);  // 3秒调用一次
     }
 
     main.onmouseout();  // 自动在main上触发一个mouseout事件方法, 使得页面进去的时候就直接开始调用轮播的功能
 
     // 遍历所有点击, 且绑定点击事件, 点击圆点切换图片
-    for(var d=0; d<len; d++){
+    for (var d = 0; d < len; d++) {
         dots[d].id = d;
-        dots[d].onclick = function() {
+        dots[d].onclick = function () {
             // alert(this.id);
             index = this.id;  // 改变index的值为当前圆点的id值
+            // 在changeImg()方法中设置其他点的className为空.
+
             changeImg();  // 调用改变图片的方法
         }
     }
@@ -51,14 +53,17 @@ function slideImg() {
 
 // 切换图片
 function changeImg() {
-    // 遍历banner下面的所有的div, 通过for循环将所有的图片隐藏
+    // 将banner下面的所有的div的display属性设置为none实现隐藏图片的功能
+    // 将dots下面的所有的span的className属性设置为空, 从而删除span标签上的active类 
     for (var i = 0; i < len; i++) {
         pics[i].style.display = "none";
+        dots[i].className = "";
     }
-    // 根据index显示对应的图片
+    // 根据index显示对应的图片和圆点
     pics[index].style.display = "block";
+    dots[index].className = "active";
 
 }
 
-
+ 
 slideImg();
